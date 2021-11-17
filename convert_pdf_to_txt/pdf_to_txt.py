3cbf94ffe3aa4af87b2a228da92681bab45f64db
Blake2912 <kcvarun29122001@gmail.com>
2021-11-17 22:00:28 +0530

    Removal of unwanted comments

--- convert_pdf_to_txt/pdf_to_txt.py
+++ convert_pdf_to_txt/pdf_to_txt.py
@@ -12,18 +12,9 @@ x=pdfreader.numPages
  
 #create a variable that will select the selected number of pages
 # pageobj=pdfreader.getPage(x-1)
+
 file1=open(r"out.txt","a")
 for i in range(x):
     pageobj=pdfreader.getPage(i)
     text=pageobj.extractText()
     file1.writelines(text)
- 
-#(x+1) because python indentation starts with 0.
-#create text variable which will store all text datafrom pdf file
- 
-#save the extracted data from pdf to a txt file
-#we will use file handling here
-#dont forget to put r before you put the file path
-#go to the file location copy the path by right clicking on the file
-#click properties and copy the location path and paste it here.
-#put "\\your_txtfilename"
