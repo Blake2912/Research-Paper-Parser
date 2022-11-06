2aae4bb790c3c22f59fdb137a8ec47c7ed31567e
Blake2912 <kcvarun29122001@gmail.com>
2022-11-06 23:40:04 +0530

    Integration progress  - Reference extraction and save it in file is complete  - Searching for reference title is intgerated

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from os import mkdir, walk
 from pdfminer.high_level import extract_text
+from sentence_transformers import SentenceTransformer, util
+from FileDetails import FileDetails
 
 import re
+import pickle
+import json
+
+
+"""
+*****************
+  UTIL FUNCTIONS
+*****************
+"""
+def get_file_list_from_folder(folder_name):
+    file_list = []
+    try:
+        for(files) in walk(str(folder_name),topdown=True):
+            file_list = files[len(files) - 1]
+            # Code for cleaning up the file list remove all files starting with ._
+            for i in file_list:
+                if i.startswith("._"):
+                    file_list.remove(i)
+        return file_list
+    except:
+        raise FileNotFoundError
+
+
 """
 ***************************
   CONVERT PDF TO TXT CODE
@@ -34,18 +59,6 @@ def save_in_txt_files(pdf_path):
     except:
         mkdir('output')
 
-def get_file_list_from_folder(folder_name):
-    file_list = []
-    try:
-        for(files) in walk(str(folder_name),topdown=True):
-            file_list = files[len(files) - 1]
-            # Code for cleaning up the file list remove all files starting with ._
-            for i in file_list:
-                if i.startswith("._"):
-                    file_list.remove(i)
-        return file_list
-    except:
-        raise FileNotFoundError
 
 """
 *************************
@@ -93,6 +106,150 @@ def get_headings_from_list(txt_file_list,txt_folder_name):
     print(heading_frequency_dict,end='\n')
 
 
+"""
+********************************************
+  EXTRACT REFERENCES AND SAVE IT IN FOLDER
+********************************************
+"""
+
+def extract_reference_from_txt_file(path_of_txt_file,source_folder_name):
+    txt_path = str(source_folder_name+"/"+path_of_txt_file)
+    txt_file = open(txt_path,'r')
+    ref_int = 0
+    references = str("references/"+path_of_txt_file)
+    try:
+        ref_file = open(r"{}".format(references),"w")
+        for i in txt_file:
+            if ref_int == 1:
+                ref_file.write(i)
+            i = i.rstrip()
+
+            if re.search(".*References|.*REFERENCES",i):
+                ref_int=1
+    except:
+        try:
+            mkdir('references')
+        except:
+            print("directory found!")
+    txt_file.close()
+
+
+"""
+********************************************
+  MATCHING REFERENCES WITH TITLE OF PAPERS
+********************************************
+"""
+# Problem Solution: https://www.sbert.net/docs/usage/semantic_textual_similarity.html
+# Sentence Transformet: https://pypi.org/project/sentence-transformers/
+
+heading_find_regex = re.compile(r': [A-Za-z]+ .*')
+number_check_regex_type_1 = re.compile(r'[0-9]+\.') # 21.
+number_check_regex_type_2 = re.compile(r'\[[0-9]+\]') # [2]
+
+def remove_starting_numbers(input_list):
+    temp_list = []
+    res_list = []
+    temp_list = input_list
+    for i in temp_list:
+        sentence = i.split(' ')
+        if number_check_regex_type_1.search(str(sentence[0])):
+            sentence.remove(sentence[0])
+            sen = " ".join(sentence)
+            res_list.append(sen)
+        elif number_check_regex_type_2.search(str(sentence[0])):
+            sentence.remove(sentence[0])
+            sen = " ".join(sentence)
+            res_list.append(sen)
+        else:
+            res_list.append(sentence)
+    
+    for i in res_list:
+        if i == ['']:
+            res_list.remove(i)
+    
+    return res_list
+
+def search_references(references_list,heading_list,model):
+    ref_temp = references_list
+    res_list = remove_starting_numbers(ref_temp)
+    # search_dict = dict()
+    for i in range(len(res_list)):
+        encode_i = model.encode(res_list[i],convert_to_tensor=True)
+        for j in heading_list:
+            cosine_scores = util.cos_sim(encode_i,j['Encoding_data'])
+            if cosine_scores[0][0] >= 0.7:
+                print("S1:{0} \nS2:{1} \n\tScore: {2}".format(res_list[i],j['Title'],cosine_scores[0][0]))
+
+def extract_references_from_file(file_path):
+    sentence_list = []
+    main_list = []
+    ref_list = []
+    match_only_list = []
+    temp = ''
+    txt_file = open(file_path,'r')
+    temp = ''.join(txt_file)
+    sentence_list = temp.splitlines(True)
+
+    # Removing extra new-line characters.
+    for i in sentence_list:
+        if i == '\n':
+            sentence_list.remove(i)
+
+    for i in sentence_list:
+        search_str = i.split(' ')[0]
+        if number_check_regex_type_1.search(str(search_str)):
+            # print(i)
+            main_list.append((i,'MATCHED'))
+            match_only_list.append(i)
+            
+        elif number_check_regex_type_2.search(str(search_str)):
+            main_list.append((i,'MATCHED'))
+        else:
+            main_list.append((i,'NOT_MATCHED'))
+    
+    ref = ''
+    for i in main_list:
+        if i[1] == 'MATCHED':
+            ref_list.append(ref)
+            ref = i[0]
+        else:
+            ref += i[0]
+    ref_list.append(ref)       
+    
+    ref_temp = []
+    for i in ref_list:
+        ref_temp.append(i.replace("\n", "").replace("\x0c",""))
+    
+    ref_list = ref_temp
+    return ref_list
+    
+
+def get_title_names(model):
+    json_file = open('REFERENCES.json')
+    data_array = json.load(json_file)
+    file_details_array = []
+    for i in data_array:
+        title_encode = model.encode(i['Title'],convert_to_tensor=True)
+        # print(title_encode)
+        file_details = FileDetails(i['Title'],i['Filename'],title_encode)
+        file_details_array.append(file_details)
+    return file_details_array
+
+# This function should run only once or whenever necessary
+def save_headings_to_pickle(model):
+    file_details_array = get_title_names(model)
+    processed_title = open('PROCESSED_REFERENCES.pkl','wb')
+    processed_list = []
+    for i in file_details_array:
+        processed_list.append({"Title": i.title,"Path": i.path, "Encoding_data": i.encoding_data})
+    pickle.dump(processed_list,processed_title)
+    processed_title.close()
+
+"""
+****************
+  MAIN FUNCTION
+****************
+"""
 if __name__ == '__main__':
     option = int(input(
         """
@@ -100,6 +257,7 @@ if __name__ == '__main__':
             1. To setup the repository, convert the pdf data files to text
             2. To extract headings and display its frequency
             3. To extract references
+            4. To find references with title names
 
         Enter the number for performing the given operation:
         """
@@ -109,11 +267,27 @@ if __name__ == '__main__':
         file_list = get_file_list_from_folder(folder_name)
         for pdf_path in file_list:
             save_in_txt_files("{0}/{1}".format(folder_name,pdf_path))
+
     elif option == 2:
         # NOTE 1: GET YOUR PDF AND TXT FILES SETUP BEFORE RUNNING THIS OPTION
         # NOTE 2: THIS IS A GENERATED FOLDER IT WILL BE GENERATED SO NEED OF CHANGING THE FOLDER NAME
         folder_name = 'output'
         file_list = get_file_list_from_folder(folder_name)
         get_headings_from_list(file_list,folder_name)
+
+    elif option == 3:
+        folder_name = 'output'
+        file_list = get_file_list_from_folder(folder_name)
+        for i in file_list:
+            extract_reference_from_txt_file(i,folder_name)
+
+    elif option == 4:
+        print("Please wait... searching in our database!")
+        model = SentenceTransformer('all-MiniLM-L6-v2')
+        a_file = open('PROCESSED_REFERENCES.pkl','rb')
+        output = pickle.load(a_file)
+        a_file.close()
+        reference_list = extract_references_from_file('references/2018Siva_OmnidirectionalMultisensoryPerceptionFusionLongTermPlaceRecognition.txt')
+        search_references(reference_list,output,model)
 
         
